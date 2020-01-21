096d0957a4fd7734dbec7c22fac20f83eab4082a
palnic-chisel <nicola.palumbo1993@gmail.com>
2020-01-21 14:31:57 +0100

    Added header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
 import './pages/homepage/HomePage';
-import HomePage from "./pages/homepage/HomePage";
 import {Route, Switch} from 'react-router-dom';
-import ShopPage from "./pages/shop/ShopPage";
 
-const HatsPage = () => (
-    <div>
-        <h1>hats page</h1>
-    </div>
-);
+import HomePage from "./pages/homepage/HomePage";
+import ShopPage from "./pages/shop/ShopPage";
+import Header from "./components/Header/Header";
+
 
 function App() {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
