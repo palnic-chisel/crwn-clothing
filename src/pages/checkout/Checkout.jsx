674cce247410fbb90294623d9df62920a326a378
palnic-chisel <nicola.palumbo1993@gmail.com>
2020-02-04 14:51:54 +0100

    stripe integrations and updating favicon and icon

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -5,6 +5,8 @@ import {createStructuredSelector} from "reselect";
 import {selectCartItems} from "../../redux/cart/cart-selectors";
 import {selectCartTotal} from "../../redux/cart/cart-selectors";
 
+import StripeCheckoutButton from "../../components/StripeButton/StripeButton";
+
 import './CheckoutStyles.scss';
 import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
 
@@ -36,6 +38,10 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+
+        <StripeCheckoutButton price={total}/>
+
+        <div className='test-warning'>Please use the following test credit card for payments<br/>4242 4242 4242 4242 - Exp: 01/20 - CVV 123</div>
     </div>
 );
 
